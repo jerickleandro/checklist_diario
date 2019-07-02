02958689d387ba50e60ae77bee1bdab94cad6b96
Erick <j.erickleandro@gmail.com>
2019-07-02 00:00:39 -0300

    Field text and button add

--- lib/main.dart
+++ lib/main.dart
@@ -20,29 +20,63 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Check list diário - GUSDEV"),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Nova tarefa",
+                          labelStyle: TextStyle(color: Colors.deepPurple)
+                      ),
+                    )
+                ),
+                RaisedButton(
+                  color: Colors.deepPurple,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: (){},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
-}
 
-Future<File> _getFile() async{
-  final directory = await getApplicationDocumentsDirectory();
-  return File("${directory.path}/data.json");
-}
 
-Future<File> _saveData() async{
+  Future<File> _getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
 
-  String data = json.encode(_toDoList);
-  final file = await _getFile();
-  return file.writeAsStringSync(data);
-}
+  Future<File> _saveData() async{
 
-Future<String> _readData() async {
-  try{
+    String data = json.encode(_toDoList);
     final file = await _getFile();
-
-    return file.readAsStringSync();
-
-  }catch (e){
-    return null;
+    return file.writeAsString(data);
   }
+
+  Future<String> _readData() async {
+    try{
+      final file = await _getFile();
+
+      return file.readAsStringSync();
+
+    }catch (e){
+      return null;
+    }
+  }
+
+
 }
