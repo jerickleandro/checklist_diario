bdcb00fc097dca584135f88ddc7b2f342e54609d
Erick <j.erickleandro@gmail.com>
2019-07-16 00:04:17 -0300

    Refresh page and ordination list by check

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,23 @@ class _HomeState extends State<Home> {
     });
   }
 
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+
+      _toDoList.sort( (a, b) {
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,11 +91,11 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
+            child: RefreshIndicator(onRefresh: _refresh, child: ListView.builder(
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
                 itemBuilder: buildItem),
-          )
+            ),)
         ],
       ),
     );
@@ -131,6 +148,7 @@ class _HomeState extends State<Home> {
                 }),
             duration: Duration(seconds: 2),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
